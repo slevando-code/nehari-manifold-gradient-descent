b87d8ec9daa395290d399b919d1cdbc8ecd04bf1
slevando-code <slevando@holycross.edu>
2026-09-12 22:10:09 -0400

    Add files via upload

--- rnewtsolver_p.m
+++ rnewtsolver_p.m
@@ -1,7 +1,7 @@
-function EDiff=rnewtsolver_p(p,b,c,A,Nx,tol,u0,delta,m,plot_last)
+function E=rnewtsolver_p(p,b,c,A,Nx,tol,u0,delta,n,plot_last)
 
 % Inexact Newton method for approximating solutions of u''''+bu''+cu=|u|^(p-1)*u
-% Sample usage E=rnewtsolver_p(3,1.8,1,50,2000,1e-10,0,1,2,1)
+% Sample usage: E=rnewtsolver_p(3,1.8,1,50,2000,1e-10,0,1,2,1)
 % Spatial interval is [-A,A], with Nx subintervals. 
 % delta = step size in gradient descent step
 % u0 = initial guess
@@ -19,19 +19,18 @@ if u==0
     u=exp(-x.^2); 
 end
 
-v= pi^4*k.^4/A^4-b*pi^2*k.^2/A^2+c;
+m = pi^4*k.^4/A^4-b*pi^2*k.^2/A^2+c;
 
 
 % Determine alpha for which P(alpha*u)=0
 N2=sum((abs(u).^(p+1)))*(A/Nx);
-dI=real(ifft(v.*fft(u)));
+dI=real(ifft(m.*fft(u)));
 I=(1/2)*sum(dI.*u)*(A/Nx);
 alpha=(2*I/N2)^(1/(p-1));
 u=alpha*u;
 
 Reserr=tol+1;
 E=[];
-EDiff=[];
 
 while (Reserr>tol)
 
@@ -41,22 +40,22 @@ f=abs(u).^(p-1).*u;
 df=p*abs(u).^(p-1);
 
 %Compute gradient of S at =u
-gradS=real(u-ifft(fft(f)./v));
+gradS=real(u-ifft(fft(f)./m));
 
 %Compute gradient of P at u
 
-gradP=real(2*u-ifft(fft(f+u.*df)./v));
+gradP=real(2*u-ifft(fft(f+u.*df)./m));
 
 %Project onto tangent space of N 
 
-w0 = gradS-real(sum(v.*fft(gradS).*conj(fft(gradP)))/sum(v.*fft(gradP).*conj(fft(gradP))))*gradP;
+w0 = gradS-real(sum(m.*fft(gradS).*conj(fft(gradP)))/sum(m.*fft(gradP).*conj(fft(gradP))))*gradP;
 w=w0;
 
 % Approximate inverse of Hessian of S at u by truncated Neumann series
 
-for i=1:m
-   J=ifft(fft(df.*w)./v);
-   J=J-real(sum(v.*fft(J).*conj(fft(gradP)))/sum(v.*fft(gradP).*conj(fft(gradP))))*gradP;
+for i=1:n
+   J=ifft(fft(df.*w)./m);
+   J=J-real(sum(m.*fft(J).*conj(fft(gradP)))/sum(m.*fft(gradP).*conj(fft(gradP))))*gradP;
    w=w0+(1-delta)*w+delta*J;
 end
 
@@ -66,25 +65,23 @@ u=u-delta*w;
 
 % Determine alpha for which P(alpha*Phi)=0
 N2=sum((abs(u).^(p+1)))*(A/Nx);
-dI=real(ifft(v.*fft(u)));
+dI=real(ifft(m.*fft(u)));
 I=(1/2)*sum(dI.*u)*(A/Nx);
 alpha=(2*I/N2)^(1/(p-1));
 
 u=alpha*u;
 
 
-LgradS=real(ifft(v.*fft(gradS)));
+LgradS=real(ifft(m.*fft(gradS)));
 
 Reserr=sqrt(sum(LgradS.*gradS)*(A/Nx));
-E=[E;Reserr];
-
 
 diff=u-Oldu;
 
-Ldiff=real(ifft(v.*fft(diff)));
+Ldiff=real(ifft(m.*fft(diff)));
 
 differr=sqrt(sum(Ldiff.*diff)*(A/Nx));
-EDiff=[EDiff;differr];
+E=[E;differr];
 
 end
 
